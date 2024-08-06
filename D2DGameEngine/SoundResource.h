2b0be10cb09085a40359ab4d633b6dc27e18e23c
dongmiki <70957529+ihaha424@users.noreply.github.com>
2024-08-06 15:56:04 +0900

    1. fmod 2. SoundResource 3. SoundSystem

---
+++ D2DGameEngine/SoundResource.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef _WIN64
+#include "fmod.hpp"
+#pragma comment (lib, "fmod_vc.lib")
+#endif
+
+#include "Resource.h"
+
+class SoundResource : public Resource<SoundResource, FMOD::Sound> {
+public:
+	SoundResource() {};
+	~SoundResource();
+
+	void LoadFile(std::wstring _key);
+	void LoadFileLoop(std::wstring _key, bool LoopCheck);
+
+	static const std::type_index TYPE_ID;
+};
+
