b958aaf81d87f5d20011c1bd69c4dc0bf554cd0c
Jiwon Choi <84387145+gamzachips@users.noreply.github.com>
2024-08-20 17:27:01 +0900

    Update DisfellPanel.h

--- Memorize/DisfellPanel.h
+++ Memorize/DisfellPanel.h
@@ -23,8 +23,6 @@ public:
 	DisfellPanel(World* _world);
 	virtual ~DisfellPanel();
 
-	virtual void Update(float _dt) override;
-
 	/**
 	 * @brief index번째 커맨드 이미지를 설정합니다.
 	 * @param index 설정할 커맨드 번째수
