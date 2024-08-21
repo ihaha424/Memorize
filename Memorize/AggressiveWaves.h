0fba43e299ccf2f524eba64bb1d7039ff62c1f70
dongmiki <70957529+ihaha424@users.noreply.github.com>
2024-08-21 18:18:16 +0900

    1. AggressiveWaves 픽스

--- Memorize/AggressiveWaves.h
+++ Memorize/AggressiveWaves.h
@@ -4,8 +4,10 @@ class AggressiveWaves : public RangeSkill
 {
 	std::vector<class AggressiveWavesEffect*> waveEffects;
 
-	float angles[4] = { -30, 30, 150, 210};
+	float angles[4] = { -30, 30, 150, 210 };
+	float SetAngles[4] = { -45, 45, 135, 180 };
 	float speed = 300.f;
+	int curIndex = 0;
 public:
 	AggressiveWaves(Actor* _owner);
 	virtual ~AggressiveWaves();
