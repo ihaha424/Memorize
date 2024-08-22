33a96e9852155f960d081bb96bd43d8462ed610e
Jiwon Choi <84387145+gamzachips@users.noreply.github.com>
2024-08-23 01:03:08 +0900

    메테오, 소실점 풀링

--- Memorize/Meteor.h
+++ Memorize/Meteor.h
@@ -2,9 +2,10 @@
 #include "RangeSkill.h"
 class Meteor : public RangeSkill
 {
-	class MeteorEffect* meteorEffect;
+	class MeteorEffect* meteorEffect[2];
 	float initialHeight = 1000.f;
 	float fallSpeed = 1500.f;
+	int nowUsingCount = 0;
 public:
 	Meteor(Actor* _owner);
 	virtual ~Meteor();
